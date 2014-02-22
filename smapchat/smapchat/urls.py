89041d55b590b91cc89db523e70077b1ff26971c
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-02-22 17:25:17 -0500

    Added event/1 and event/1.json routes

--- smapchat/smapchat/urls.py
+++ smapchat/smapchat/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^event/1', TemplateView.as_view(template_name="map.html")),
+    url(r'^event/1.json', 'smapchat.views.event_json', name='event_json')
 )
---
+++ smapchat/smapchat/views.py
@@ -0,0 +1,15 @@
+import json
+
+from django.http import HttpResponse
+
+
+def event_json(request):
+    obj = {
+        'id': 1,
+        'name': "McHacks",
+        'desc': 'A Hackathon at McGill',
+        'maps': [
+            {'name': 'ssmu-floor-1', 'source': 'img'}
+        ]
+    }
+    return HttpResponse(json.dumps(obj), content_type="application/json")
