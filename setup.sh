c3dc4cdfa1f3b21376468c57243238604c63e0ef
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-02-22 16:12:50 -0500

    Added wip/

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ sudo add-apt-repository ppa:pitti/postgresql
 sudo apt-get update
 sudo apt-cache search ^postgresql-9 
 sudo apt-get install postgresql-9.1
+sudo apt-get install libpq-dev
 sudo apt-get install python-dev
