aed9dc6a7929aeea1bc785b2e2bf7bdba575996b
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-02-22 18:23:22 -0500

    Added bootstrap.

--- smapchat/smapchat/urls.py
+++ smapchat/smapchat/urls.py
@@ -3,11 +3,14 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+from .views import HomePageView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'smapchat.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^event/1$', TemplateView.as_view(template_name="map.html")),
     url(r'^event/1.json$', 'smapchat.views.event_json', name='event_json')
