0b671d20fcc670bc6820c5fe1797dd42a98802df
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2014-02-22 17:58:03 -0500

    Fixed URLs to include $ for end of URL.

--- smapchat/smapchat/urls.py
+++ smapchat/smapchat/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^event/1', TemplateView.as_view(template_name="map.html")),
-    url(r'^event/1.json', 'smapchat.views.event_json', name='event_json')
+    url(r'^event/1$', TemplateView.as_view(template_name="map.html")),
+    url(r'^event/1.json$', 'smapchat.views.event_json', name='event_json')
 )
